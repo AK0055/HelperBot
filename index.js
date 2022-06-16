5622679c6a8373c467dcc645169fb5a09d2d4be0
Ajaykumar M <m1ajay2001@gmail.com>
2022-06-16 20:00:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const app = express();
-const product = require("./bot");
+const boter = require("./bot");
 app.use(express.json({ extended: false }));
 
-app.use("/", product);
+app.use("/", boter);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {console.log(`Server is running in port ${PORT}`)
 });
